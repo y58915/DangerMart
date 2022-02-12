ec91b66781917cb4a2f95005e4f75182f2a36d04
MauricioMirandaDev <32001207+MauricioMirandaDev@users.noreply.github.com>
2022-02-11 19:01:55 -0800

    Implemented Collection Areas
    Created a class for collection areas. Collection areas now have items that are instantiated.

---
+++ Assets/__Scripts/CollectionAreas/CollectionArea.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionArea : MonoBehaviour
+{
+    [Header("Item")]
+    [SerializeField]
+    private Item item;
+
+    private GameObject myItem;
+    private List<GameObject> inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myItem = Instantiate(item.itemModelPrefab, this.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Add this collection area's item to the player's inventory 
+    public void AddToInventory(List<GameObject> playerInventory)
+    {
+        playerInventory.Add(myItem);
+
+        Destroy(myItem);
+        myItem = Instantiate(item.itemModelPrefab, this.transform.position, Quaternion.identity);
+    }
+}
