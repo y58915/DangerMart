6f21993a0b20326bacb13d69d56639e974962f77
Jiashuo Tong <jiashuotong@jiashuos-mbp.lan>
2022-03-25 13:32:22 -0700

    Added enemy patrolling behavior & routes

---
+++ Assets/PatrollingEnemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollingEnemy : MonoBehaviour
+{
+    NavMeshAgent enemy;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        waypointIndex = 0;
+        UpdateDestination();
+        Debug.LogFormat("Start: Position => " + transform.position);
+        Debug.LogFormat("Start: Target => " + target);
+        Debug.LogFormat("Start: Index => " + waypointIndex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.LogFormat("Updating Distance" + Vector3.Distance(transform.position, target));
+        // Debug.LogFormat("Updating Index" + waypointIndex);
+
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            Debug.LogFormat("Update: Position => " + transform.position);
+            Debug.LogFormat("Update: Target => " + target);
+
+            IterateWaypointIndex();
+            Debug.LogFormat("Update: Index => " + waypointIndex);
+            // Debug.LogFormat("Iterate Waypoint Index" + waypointIndex);
+
+            UpdateDestination();
+        }
+    }
+
+    void UpdateDestination() 
+    {
+       target = waypoints[waypointIndex].position;
+       enemy.SetDestination(target);
+    }
+
+    void IterateWaypointIndex()
+    {
+        waypointIndex ++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
